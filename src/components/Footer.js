788de40879362bcfcb0983bb743110c0e606f255
Wasan Subaihi <wasan.subaihi16@gmail.com>
2024-05-25 18:55:07 +0300

    last updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
-import "./FooterStyles.css"
+import "./FooterStyles.css";
 
-import React from 'react'
-import { FaPhone, FaMailBulk, FaLinkedin, FaFacebook, FaTwitter } from "react-icons/fa";
+import React from 'react';
+import { FaMailBulk, FaLinkedin, FaFacebook, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -9,16 +9,26 @@ const Footer = () => {
       <div className="footer-container">
         <div className="left">
           <div className="email">
-            <h4><FaMailBulk size={20} style={{ color: "#000", marginRight: "2rem" }} /> itfolio.ask@gmail.com </h4>
+            <h4>
+            <a href="mailto:itfolio.ask@gmail.com" className="email-link">
+  <FaMailBulk size={20} style={{ color: "#000", marginRight: "10px" }} /> itfolio.ask@gmail.com
+</a>
+            </h4>
           </div>
         </div>
         <div className="right">
           <h4>About the web Application</h4>
-          <p className="footerp">Thanks for visiting! This web application was created by Wasan Subaihi, Lara Al-Qaisi, Shadan Ekedat, and Hamza Al-Manaseer. We're always open to hear your ideas , recommendations ,  thoughts or suggestions with us for making it even better.</p>
+          <p className="footerp">Thanks for visiting! This web application was created by Wasan Subaihi, Lara Al-Qaisi, Shadan Ekedat, and Hamza Al-Manaseer. We're always open to hear your ideas, recommendations, thoughts, or suggestions with us for making it even better.</p>
           <div className="social">
-            <FaFacebook size={30} style={{ color: "#000", marginRight: "1rem" }} />
-            <FaTwitter size={30} style={{ color: "#000", marginRight: "1rem" }} />
-            <FaLinkedin size={30} style={{ color: "#000", marginRight: "1rem" }} />
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaFacebook size={30} style={{ color: "#000", marginRight: "1rem" }} />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaTwitter size={30} style={{ color: "#000", marginRight: "1rem" }} />
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaLinkedin size={30} style={{ color: "#000", marginRight: "1rem" }} />
+            </a>
           </div>
         </div>
       </div>
@@ -28,7 +38,7 @@ const Footer = () => {
         <p>All rights reserved © 2024</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
